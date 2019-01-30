d51ab06673c33196d53505ca67bbdf2ee7aa4b80
Jarrad Miller <jm020801@gmail.com>
2019-01-30 14:33:33 -0700

    setup + tests for control.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,42 @@
 // Test away!
+import React from "react";
+import { render } from "react-testing-library";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+import Controls from "./Controls";
+
+describe("Control test", () => {
+    it("should render lock or closed gate", () => {
+      const { getByText } = render(<Controls locked={false} closed={false} />);
+  
+      getByText(/lock gate/i);
+      getByText(/close gate/i);
+    });
+  });
+  
+  describe("Control test", () => {
+    it("should render unlock or open gate", () => {
+      const { getByText } = render(<Controls locked={true} closed={true} />);
+  
+      getByText(/unlock gate/i);
+      getByText(/open gate/i);
+    });
+  });
+  
+  describe("Control test", () => {
+    it("should render unlock or close gate", () => {
+      const { getByText } = render(<Controls locked={false} closed={true} />);
+  
+      getByText(/open gate/i);
+      getByText(/lock gate/i);
+    });
+  });
+  
+  describe("Control test", () => {
+    it("should render lock or open gate", () => {
+      const { getByText } = render(<Controls locked={true} closed={false} />);
+  
+      getByText(/lock gate/i);
+      getByText(/unlock gate/i);
+    });
+  });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+import Dashboard from "./Dashboard";
+
+describe("Locked", () => {
+  it("should locke gate", () => {
+    const { getByTestId } = render(<Dashboard />);
+  });
+});
